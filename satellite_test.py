92b1b6f5cbb3d12507e8f0e5fcf5055da94ed5b8
DavidJones011 <jones.david0911@gmail.com>
2021-09-25 01:03:07 -0600

    added satellite data tester script

---
+++ satellite_test.py
@@ -0,0 +1,98 @@
+import sys
+import math
+import os
+import re
+import decimal
+
+def main() :
+
+    line = sys.stdin.readline()
+    tokens = re.split('\s|,', line)
+    #remove whitespace
+    while("" in tokens) :
+        tokens.remove("")
+    if(len(tokens) != 2) :
+        sys.stdout.write("Input was invalid.\n")
+        return
+
+    thisSatellites = getSatelliteData(tokens[0])
+    otherSatellites = getSatelliteData(tokens[1])
+
+    if(len(thisSatellites) != len(otherSatellites)) :
+        sys.stdout.write("Number of satellites mismatched!\n")
+
+
+    success = True
+    for i in range(0, len(thisSatellites)) :
+
+        tIndex = thisSatellites[i][0]
+        oIndex = otherSatellites[i][0]
+
+        if(tIndex != oIndex) :
+            sys.stdout.write("Satellite index mismatch! {} and {}\n".format(tIndex, oIndex))
+            success = False
+            break
+
+        tLoc = thisSatellites[i][2]
+        oLoc = otherSatellites[i][2]
+
+        if(magnitude(subVectors(tLoc, oLoc)) > 0.01) :
+            sys.stdout.write("Satellite locations differ further than a centimeter! {} and {}\n".format(tIndex, oIndex))
+            success = False
+            break
+
+        tTime = thisSatellites[i][1]
+        oTime = otherSatellites[i][1]
+
+        if(abs(tTime - oTime) <= 1E-11) :
+            sys.stdout.write("Satellite times differ too much! {} and {}\n".format(tIndex, oIndex))
+            success = False
+            break
+
+        pass
+
+    if success :
+        sys.stdout.write("Success! The results were close.\n")
+    else :
+        sys.stdout.write("Failed! :(\n")
+
+    pass
+
+def getSatelliteData(fileName) :
+    data_file = open(os.path.join(sys.path[0], fileName), "r")
+    satellites = list()
+    i = 0
+    for line in data_file.readlines() :
+
+        if line == '':
+            break
+
+        tokens = re.split('\s|,', line)
+
+        # remove empty strings from the list of tokens
+        while("" in tokens) :
+            tokens.remove("")
+
+        if(len(tokens) == 0) :
+            break
+
+        s = [0, 0, [0,0,0]]
+        s[0]    = int(tokens[0])
+        s[1]    = decimal.Decimal(tokens[1])
+        s[2][0] = decimal.Decimal(tokens[2])
+        s[2][1] = decimal.Decimal(tokens[3])
+        s[2][2] = decimal.Decimal(tokens[4])
+        satellites.append(s)
+        i = i + 1
+
+    data_file.close()
+
+    return satellites
+
+def subVectors(u,v) :
+    return [u[0] - v[0], u[1] - v[1], u[2] - v[2]]
+
+def magnitude(u):
+    return decimal.Decimal(math.sqrt((u[0] * u[0]) + (u[1] * u[1]) + (u[2] * u[2])))
+
+main()
