38319f9d84082d04f50f447b555ccadc8f2a019b
DavidJones011 <jones.david0911@gmail.com>
2021-11-06 01:19:06 -0600

    satellite.py and satellite_test.py now use numpy instead of mathmp

--- satellite.py
+++ satellite.py
@@ -1,11 +1,10 @@
 import sys
 import os
 import re
-import mpmath as mp
+import numpy as np
+from numpy import linalg
 
 def main() :
-    # set the decimal place for calculations
-    mp.mp.dps = 17
     # create and set global variables
     global pi, s, c, r
     pi, s, c, r, satellites = getData()
@@ -22,16 +21,16 @@ def main() :
             break
 
         #store the tokenized data into the correct data types
-        t_v      = mp.mpf(tokens[0])
+        t_v      = np.float64(tokens[0])
         psi_d    = int(tokens[1])
         psi_m    = int(tokens[2])
-        psi_s    = mp.mpf(tokens[3])
+        psi_s    = np.float64(tokens[3])
         ns_value = int(tokens[4])
         lambda_d = int(tokens[5])
         lambda_m = int(tokens[6])
-        lambda_s = mp.mpf(tokens[7])
+        lambda_s = np.float64(tokens[7])
         ew_value = int(tokens[8])
-        altitude = mp.mpf(tokens[9])
+        altitude = np.float64(tokens[9])
 
         # get the cartesian coordinates of the vehicle
         latitude = degreeToRad(psi_d, psi_m, psi_s, ns_value)
@@ -39,13 +38,13 @@ def main() :
         x_v = sphericalToCartesian(r + altitude, longitude, latitude)
         x_v = rotate(x_v, t_v)
 
-        dot_x_v = dotProduct(x_v,x_v)
+        dot_x_v = np.dot(x_v,x_v)
 
         for i in range(0,24) :
             # use newtons method to find satellite position and time
             x_s, t_s = satelliteTimeAndLocOnSend(satellites, i, x_v, t_v)
 
-            if dotProduct(x_v, x_s) > dot_x_v :
+            if np.dot(x_v, x_s) > dot_x_v :
                 output_file.write("{} {} {} {} {}\n".format(i, t_s, x_s[0], x_s[1], x_s[2]))
                 sys.stdout.write("{} {} {} {} {}\n".format(i, t_s, x_s[0], x_s[1], x_s[2]))
             
@@ -58,7 +57,7 @@ def getData() :
     # initialize satellite data
     satellites = list()
     for i in range(0, 24):
-        satellites.append([[0,0,0],[0,0,0],0,0,0])
+        satellites.append([np.array([0.0,0.0,0.0]), np.array([0.0,0.0,0.0]), 0.0, 0.0, 0.0])
 
     data_file = open(os.path.join(sys.path[0], 'data.dat'), "r")
     for line in data_file.readlines() :
@@ -76,68 +75,68 @@ def getData() :
             break
 
         if(tokens[2] == 'pi') :
-            pi = mp.mpf(tokens[0])
+            pi = np.float64(tokens[0])
         elif(tokens[2] == 's') :
-            s = mp.mpf(tokens[0])
+            s = np.float64(tokens[0])
         elif(tokens[2] == 'c') :
-            c = mp.mpf(tokens[0])
+            c = np.float64(tokens[0])
         elif(tokens[2] == 'R') :
-            r = mp.mpf(tokens[0])
+            r = np.float64(tokens[0])
         elif(tokens[2] == 'u1') :
-            satellites[int(tokens[5])][0][0] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][0][0] = np.float64(tokens[0])
         elif(tokens[2] == 'u2') :
-            satellites[int(tokens[5])][0][1] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][0][1] = np.float64(tokens[0])
         elif(tokens[2] == 'u3') :
-            satellites[int(tokens[5])][0][2] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][0][2] = np.float64(tokens[0])
         elif(tokens[2] == 'v1') :
-            satellites[int(tokens[5])][1][0] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][1][0] = np.float64(tokens[0])
         elif(tokens[2] == 'v2') :
-            satellites[int(tokens[5])][1][1] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][1][1] = np.float64(tokens[0])
         elif(tokens[2] == 'v3') :
-            satellites[int(tokens[5])][1][2] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][1][2] = np.float64(tokens[0])
         elif(tokens[2] == 'periodicity') :
-            satellites[int(tokens[5])][2] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][2] = np.float64(tokens[0])
         elif(tokens[2] == 'altitude') :
-            satellites[int(tokens[5])][3] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][3] = np.float64(tokens[0])
         elif(tokens[2] == 'phase') :
-            satellites[int(tokens[5])][4] = mp.mpf(tokens[0])
+            satellites[int(tokens[5])][4] = np.float64(tokens[0])
 
     data_file.close()
     return pi, s, c, r, satellites
 
 # converts degree (geodesic) to radians
 def degreeToRad(angle, angle_minute, angle_second, dir_value) :
-    absolute_angle = angle + mp.mpf(angle_minute / 60.0) + mp.mpf(angle_second / 3600.0)
+    absolute_angle = angle + (np.float64(angle_minute) / 60.0) + (np.float64(angle_second) / 3600.0)
     return absolute_angle * (pi / 180.0) * dir_value
 
 # get cartesian coords from spherical coords
 def sphericalToCartesian(radius, angle_lambda, angle_phi) :
-    x = radius * mp.cos(angle_lambda) * mp.cos(angle_phi)
-    y = radius * mp.sin(angle_lambda) * mp.cos(angle_phi)
-    z = radius * mp.sin(angle_phi)
-    return [x, y, z]
+    x = radius * np.cos(angle_lambda) * np.cos(angle_phi)
+    y = radius * np.sin(angle_lambda) * np.cos(angle_phi)
+    z = radius * np.sin(angle_phi)
+    return np.array([x, y, z])
 
 # get the cartesian coordinates of satellite
 def getSatelliteLocation(satellites, index, t) :
-    angle = mp.mpf((2.0 * pi * t) / satellites[index][2])
+    angle = 2.0 * pi * t / satellites[index][2]
     coeff = r + satellites[index][3]
-    u = scaleVector(mp.cos(angle + satellites[index][4]), satellites[index][0])
-    v = scaleVector(mp.sin(angle + satellites[index][4]), satellites[index][1])
-    return scaleVector(coeff, addVectors(u,v))
+    u = satellites[index][0] * np.cos(angle + satellites[index][4])
+    v = satellites[index][1] * np.sin(angle + satellites[index][4])
+    return (u + v) * coeff
 
 # get the satellites velocity
 def getSatelliteVelocity(satellites, index, t) :
     coeff = (2.0 * pi * (r + satellites[index][3])) / satellites[index][2]
     angle = (2.0 * pi * t) / satellites[index][2]
-    u = scaleVector(-1.0 * mp.sin(angle + satellites[index][4]), satellites[index][0])
-    v = scaleVector(mp.cos(angle + satellites[index][4]), satellites[index][1])
-    return scaleVector(coeff, addVectors(u,v))
+    u = satellites[index][0] * -np.sin(angle + satellites[index][4])
+    v = satellites[index][1] * np.cos(angle + satellites[index][4])
+    return (u + v) * coeff
 
 # get the satellite location and time to send the signal at x_v at time t
 def satelliteTimeAndLocOnSend(satellites, index, x_v, t_v) :
     x0_s = getSatelliteLocation(satellites, index, t_v)
     # calculate t0
-    prev_t = t_v - (magnitude(subVectors(x0_s, x_v)) / c)
+    prev_t = t_v - (np.linalg.norm(x0_s - x_v) / c)
     threshold = 0.01 / c
     max_iterations = 200
     best_t = 0
@@ -147,14 +146,13 @@ def satelliteTimeAndLocOnSend(satellites, index, x_v, t_v) :
         pos = getSatelliteLocation(satellites, index, prev_t)
 
         # get f(x)
-        diff = subVectors(pos, x_v)
-        f = dotProduct(diff, diff) - ((c**2) * ((t_v - prev_t)**2))
+        diff = pos - x_v
+        f = np.dot(diff, diff) - (np.power(c, 2) * (np.power(t_v - prev_t, 2)))
         # get f'(x)
-        f_prime = (2 * dotProduct(subVectors(pos, x_v), getSatelliteVelocity(satellites, index, prev_t))) + (2.0 * (c**2) * (t_v - prev_t))
-
+        f_prime = (2 * np.dot(diff, getSatelliteVelocity(satellites, index, prev_t))) + (2.0 * np.power(c, 2) * (t_v - prev_t))
         cur_t = prev_t - (f / f_prime)
 
-        if abs(cur_t - prev_t) < threshold :
+        if np.abs(cur_t - prev_t) < threshold :
             best_t = cur_t
             break
 
@@ -163,34 +161,34 @@ def satelliteTimeAndLocOnSend(satellites, index, x_v, t_v) :
     return getSatelliteLocation(satellites, index, best_t), best_t
 
 # returns a vector that is scaled
-def scaleVector(scale, vec) :
+#def scaleVector(scale, vec) :
     x = vec[0] * scale
     y = vec[1] * scale
     z = vec[2] * scale
     return [x, y, z]
 
 # returns the magnitude of u
-def magnitude(u):
+#def magnitude(u):
     return mp.sqrt((u[0] * u[0]) + (u[1] * u[1]) + (u[2] * u[2]))
 
 # returns the dot product of two vectors u and v
-def dotProduct(u, v) :
+#def dotProduct(u, v) :
     return (u[0] * v[0]) + (u[1] * v[1]) + (u[2] * v[2])
 
 # adds two vectors
-def addVectors(u,v) :
+#def addVectors(u,v) :
     return [u[0] + v[0], u[1] + v[1], u[2] + v[2]]
 
 # subtracts two vectors
-def subVectors(u,v) :
+#def subVectors(u,v) :
     return [u[0] - v[0], u[1] - v[1], u[2] - v[2]]
 
 # rotates a point along the z-axis
 def rotate(u, t_v) :
     time_offset = (2*pi*t_v) / s
-    v = [(mp.cos(time_offset) * u[0]) + (-mp.sin(time_offset) * u[1]),
-         (mp.sin(time_offset) * u[0]) + (mp.cos(time_offset) * u[1]),
-         (u[2])]
+    v = np.array([(np.cos(time_offset) * u[0]) + (-np.sin(time_offset) * u[1]),
+         (np.sin(time_offset) * u[0]) + (np.cos(time_offset) * u[1]),
+         (u[2])])
     return v
 
 main()
--- satellite_test.py
+++ satellite_test.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import re
-from decimal import *
+import numpy as np
 
 def main() :
 
@@ -38,13 +38,13 @@ def main() :
         tLoc = thisSatellites[i][2]
         oLoc = otherSatellites[i][2]
 
-        if(magnitude(subVectors(tLoc, oLoc)) > 0.01) :
+        if(np.linalg.norm(tLoc - oLoc) > 0.01) :
             sys.stdout.write("Satellite locations differ further than a centimeter! {} and {}\n".format(tIndex, oIndex))
             success = False
             break
 
-        tTime = thisSatellites[i][1]
-        oTime = otherSatellites[i][1]
+        #tTime = thisSatellites[i][1]
+        #oTime = otherSatellites[i][1]
 
         # could test for accuracy in seconds
         #if(abs(tTime - oTime) <= 1E-11) :
@@ -81,21 +81,13 @@ def getSatelliteData(fileName) :
 
         s = [0, 0, [0,0,0]]
         s[0]    = int(tokens[0])
-        s[1]    = Decimal(tokens[1])
-        s[2][0] = Decimal(tokens[2])
-        s[2][1] = Decimal(tokens[3])
-        s[2][2] = Decimal(tokens[4])
+        s[1]    = np.float64(tokens[1])
+        s[2] = np.array([np.float64(tokens[2]), np.float64(tokens[3]), np.float64(tokens[4])])
         satellites.append(s)
         i = i + 1
 
     data_file.close()
 
     return satellites
-
-def subVectors(u,v) :
-    return [u[0] - v[0], u[1] - v[1], u[2] - v[2]]
-
-def magnitude(u):
-    return Decimal(Decimal.sqrt((u[0] * u[0]) + (u[1] * u[1]) + (u[2] * u[2])))
 
 main()
